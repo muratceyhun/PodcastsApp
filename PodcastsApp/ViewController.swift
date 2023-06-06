e346032d8de3fd93ab6251f739c54051782f0b63
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-06-06 19:34:00 +0300

    essentials.

---
+++ PodcastsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastsApp
+//
+//  Created by Murat Ceyhun Korpeoglu on 6.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
