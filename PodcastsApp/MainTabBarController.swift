3eb01bf76ea36237f8b4e6f9474e3614b9b8c831
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-06-06 23:56:37 +0300

    some setup and helper functions has been created.

---
+++ PodcastsApp/MainTabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  MainTabBarController.swift
+//  PodcastsApp
+//
+//  Created by Murat Ceyhun Korpeoglu on 6.06.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewControllers()
+    }
+    
+    //MARK: - Setup Functions
+    fileprivate func setupViewControllers() {
+        tabBar.tintColor = .purple
+        viewControllers = [generateNavController(with: ViewController(), title: "Favorites", imageName: "favorites"),
+                           generateNavController(with: ViewController(), title: "Search", imageName: "search"),
+                           generateNavController(with: ViewController(), title: "Downloads", imageName: "downloads")
+        ]
+    }
+    
+    //MARK: - Helper Functions
+    fileprivate func generateNavController(with rootViewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.image = UIImage(named: imageName)
+        
+        return navController
+    }
+}
