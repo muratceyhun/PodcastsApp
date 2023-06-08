6491511c0a9a8215927c7e61e0ea8f72eccb9590
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-06-08 23:09:41 +0300

    Storyboard deleted and alamofire request tested successfuly

--- PodcastsApp/Controllers/PodcastsSearchController.swift
+++ PodcastsApp/Controllers/PodcastsSearchController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
     
@@ -36,7 +37,20 @@ class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+        
+        let url = "https://itunes.apple.com/search?term=\(searchText))"
+        
+        AF.request(url).responseData { dataResponse in
+            if let error = dataResponse.error {
+                print("ERROR: \(error)")
+                return
+            }
+            
+            guard let data = dataResponse.data else {return}
+            let dummyString = String(data: data, encoding: .utf8)
+            print(dummyString)
+        }
+
     }
     
     
