5286557d98d0d31b47ecaada320d33a729e14f42
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-06-21 21:23:27 +0300

    Time Slider, Fast Forward, Rewind and Volume Controls features has been added.

--- PodcastsApp/Extensions/CMTime.swift
+++ PodcastsApp/Extensions/CMTime.swift
@@ -13,7 +13,7 @@ extension CMTime {
     func toDisplayString() -> String {
         
         let totalSeconds = Int(CMTimeGetSeconds(self))
-         let seconds = totalSeconds % 60
+        let seconds = totalSeconds % 60
         let minutes = totalSeconds / 60
         let timeFormatString = String(format: "%02d:%02d", minutes, seconds)
         return timeFormatString
